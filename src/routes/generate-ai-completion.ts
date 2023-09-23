6c19e267a65bb089f00abd0a833ad60e1c066991
Rafael Borges <rafael.borges2698@gmail.com>
2023-09-23 19:46:00 -0300

    added ai to use lib from ai sream, altered routes

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import z from 'zod'
-import { createReadStream } from "node:fs"
+import { streamToResponse, OpenAIStream } from 'ai'
 import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
 
@@ -9,12 +9,12 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
         
         const bodySchema = z.object({
             videoId: z.string().uuid(),
-            template: z.string(),
+            prompt: z.string(),
             temperature: z.number().min(0).max(1).default(0.5)
         })
 
 
-        const { videoId, template, temperature } = bodySchema.parse(request.body)
+        const { videoId, prompt, temperature } = bodySchema.parse(request.body)
 
         const video = await prisma.video.findUniqueOrThrow({
             where: {
@@ -26,18 +26,29 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
             return response.status(400).send({ error: 'Video transcription was not generated yet.' })
         }
 
-        const promptMessage = template.replace('{transcription}', video.transcription) //adicionar a transcrição
+        const promptMessage = prompt.replace('{transcription}', video.transcription) //adicionar a transcrição
 
         const responseOpenAiCreateText = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo-16k', //versão free mas aceita menos tokens (menos caracteres de env-io e retorno)
             temperature,
             messages: [
                 { role: 'user', content: promptMessage}
-            ]
-
+            ],
+            stream: true, //gerar uma linha de cada vez
         })
 
-        return responseOpenAiCreateText
+        //para gerar renpose linha de cada vez pra nao gerar tudo de uma vez
+
+        const stream = OpenAIStream(responseOpenAiCreateText)
+
+        streamToResponse(stream, response.raw, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        
+            }
+        })
+        //return responseOpenAiCreateText
 
     })
 }
